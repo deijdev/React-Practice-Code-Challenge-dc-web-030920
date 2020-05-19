893c37b396a064687e2d24f4c02bfa86e5fd3761
Deijah Wilson <deijahwilson1@gmail.com>
2020-05-19 17:51:49 -0400

    fix remove sushi function, need to add plates function, need to add exceeding amount function

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,57 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  constructor(){
+    super()
+    this.state={
+      sushiList : [],
+      cost: 0, 
+      eaten: []  
+    }
+  }
+
+ 
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushiArray => {
+      this.setState({
+        sushiList : sushiArray.splice(0, 4)
+      })
+    })
+  }
+
+  show4More = () => {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushiArray => {
+      const fourthSushi = this.state.sushiList[3]
+      const n = sushiArray.findIndex(sushi => sushi.id === fourthSushi.id)
+      const newArray = sushiArray.slice(n)
+      this.setState({
+        sushiList : newArray.splice(0, 4)
+      })
+    })
+  }
+
+  removeSushi=(e, selectedSushi)=>{
+    const price = selectedSushi.price
+    const toBeDeleted = this.state.sushiList.find(sushi => sushi.id === selectedSushi.id)
+    this.setState({
+      eaten: this.state.eaten.concat(selectedSushi),
+      cost: this.state.cost + price,
+      sushiList: this.state.sushiList
+      
+    })
+    // debugger
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushiList={this.state.sushiList} show4More={this.show4More} removeSushi={this.removeSushi}/>
+        <Table sushiList={this.state.sushiList} cost={this.state.cost}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,19 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi.js'
 
 const SushiContainer = (props) => {
+
+  const sushiComponent = props.sushiList.map(sushi =>{
+    return <Sushi sushi={sushi} key={sushi.id} removeSushi={props.removeSushi}/>
+  })
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {sushiComponent}
+        <br></br>
+        <MoreButton show4More={props.show4More}/>
       </div>
     </Fragment>
   )
